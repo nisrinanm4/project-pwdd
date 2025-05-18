270fd383d1b35eb2a631a43f090f1414c17cb810
nisrinanm4 <ririnbantul@gmail.com>
2025-05-18 21:31:16 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "website";
+
+$conn = mysqli_connect('localhost', 'root', '', 'website') or die('connection failed');
+?>
